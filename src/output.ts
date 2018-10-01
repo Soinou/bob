07cb8922af038f6359bf59ca0d2868e3c8d58a1f
François DANGUY <informatique@te61.fr>
2018-10-01 11:48:04 +0200

    welp

--- src/output.ts
+++ src/output.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 export function output(target: string, production: boolean, directory: string, publicPath?: string) {
     if (target === "web") {
         return {
-            filename: production ? "[name].[chunkhash].js" : "[name].js",
+            filename: production ? "[name].[contenthash].js" : "[name].js",
             path: path.resolve(directory),
             publicPath,
         };
