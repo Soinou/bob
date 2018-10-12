ab07647ff33eab22fe92831c54c5d19bf9aab039
François DANGUY <informatique@te61.fr>
2018-10-12 10:43:25 +0200

    Too used to typescript types

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -4,8 +4,8 @@ export function analyzer(serve: boolean, port: number = 8888) {
     return new BundleAnalyzerPlugin({
         analyzerHost: "0.0.0.0",
         analyzerMode: serve ? "server" : "static",
+        analyzerPort: port,
         logLevel: "error",
         openAnalyzer: false,
-        port,
     });
 }
